8dd3b7247e308b55ba9f0a64d90bc6bf3b9dd844
Sam <101374@glr.nl>
2025-03-10 22:20:33 +0100

    Aanpassingen

---
+++ script.js
@@ -0,0 +1,9 @@
+// ScrollReveal
+ScrollReveal().reveal('#skills .reveal', {
+    viewFactor: 0.3, // Element wordt pas getoond als 30% zichtbaar is
+    delay: 200,
+    duration: 800,
+    distance: '50px',
+    origin: 'bottom'
+  });
+  
